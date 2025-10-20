40d348d98e2f8c02b552b39c53c699170547143f
Lucas <Devantier2002@gmail.com>
2025-10-20 20:55:30 -0300

    Feat: Add description ~

--- src/utils/VendaType.ts
+++ src/utils/VendaType.ts
@@ -1,10 +1,16 @@
-import type { OculosType } from "./OculosType"
-
 export type VendaType = {
   id: number;
   clienteId: string;
   carroId: number;
-  oculos: OculosType;
+  // inline oculos shape so components don't need to import OculosType
+  oculos?: {
+    id?: number;
+    nome?: string;
+    tipo?: string;
+    preco?: number | string;
+    foto?: string;
+    [key: string]: any;
+  } | null;
   descricao: string;
   resposta: string | null;
   createdAt: string;
